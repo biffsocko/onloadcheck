e9754bc79fbb2e0222f168f1b89bf955861c3d1c
TR Murphy <biffsocko@gmail.com>
2023-05-13 13:38:48 -0400

    Update onloadchk.sh

--- onloadchk.sh
+++ onloadchk.sh
@@ -1,29 +1,13 @@
 #!/bin/sh
-
-###############################################
-# OLD
-###############################################
-#list="ny4prodfh1
-#      ny11prodmajor1
-#      uat1"
-#for next in $list
-
-#do
-#     echo "trying $next"
-#     ssh -q -o ConnectTimeout=3  $next "onload --version" > /dev/null 2>&1
-#     if [ $? -ne 0 ]
-#     then
-#         echo "$next failed"
-#     fi
-#done
-
-
-###############################################
-# NEW
-###############################################
+###########################################################
+# TR Murphy
+# onloadcheck
+#
+#  add your own webhook here
+###########################################################
 LIST="/onload_list"
 MAILLIST="thomas.murphy@stonex.com,jason.alexander@stonex.com"
-WEBHOOK="https://intlfcstone.webhook.office.com/webhookb2/a0ae8289-3917-441f-a9fe-356652fd619d@d1bdddc1-de7b-4a77-914e-213d203667f2/IncomingWebhook/cda8e7dc7c494adf8f173f814fdb5dcd/cf7cb2e8-d857-424f-9700-516f06c11637"
+WEBHOOK="https://yourdomain.webhook.office.com/webhookb2/bigstringhere"
 
 #    curl -H 'Content-Type: application/json' -d '{"text": "Failed to write database to WORM Drive for compliance - ERROR 1"}' ${WEBHOOK}
 
